971421d22e7bf4fa9b1538a833b4332187e6c425
Mars Kim <marsinearth@gmail.com>
2020-06-10 21:57:52 +0900

    comment out onLoadMore which is not being used, edit slotmachine focus area css stylings

--- src/components/slotMachine.js
+++ src/components/slotMachine.js
@@ -1,12 +1,15 @@
-import React, { PureComponent } from 'react'
-import { View, ScrollView, StyleSheet, Keyboard } from 'react-native'
-import throttle from 'lodash.throttle'
-import Picker from 'rmc-picker/es/Picker'
-import Popup from 'rmc-picker/es/Popup'
-import 'rmc-picker/assets/index.css'
-import 'rmc-picker/assets/popup.css'
-import { BottomButton, item } from './App'
-import { MAX_ITEM_LIST_NUM } from '../utils'
+import "rmc-picker/assets/index.css";
+import "rmc-picker/assets/popup.css";
+
+import { BottomButton, item } from "./App";
+import { Keyboard, ScrollView, StyleSheet, View } from "react-native";
+import React, { PureComponent } from "react";
+
+import { MAX_ITEM_LIST_NUM } from "../utils";
+import Picker from "rmc-picker/es/Picker";
+import Popup from "rmc-picker/es/Popup";
+
+// import throttle from "lodash.throttle";
 
 // TODO: gotta deal with the structure of this.state.items. you can't deal with the negative labels in current structure
 
@@ -14,20 +17,20 @@ type PickerCompProps = {
   items: item[],
   value: string,
   onChangeValue: () => void,
-  onLoadMore: () => void
-}
+  onLoadMore: () => void,
+};
 
 type slotMachineProps = {
   option: string,
   items: item[],
   numRange: number[],
   disabled: boolean,
-  answer: () => void
-}
+  answer: () => void,
+};
 
 const generateArray = async ({ data, startIdx, endIdx, limit, num, more }) => {
-  let init = endIdx >= startIdx ? 0 : num
-  const generatedArray = []
+  let init = endIdx >= startIdx ? 0 : num;
+  const generatedArray = [];
   for (let j = 0; j <= limit; j++) {
     for (
       let i = endIdx >= startIdx ? 0 : num;
@@ -36,34 +39,39 @@ const generateArray = async ({ data, startIdx, endIdx, limit, num, more }) => {
     ) {
       generatedArray.push({
         label: more ? `more${init}` : init,
-        datum: data[i]
+        datum: data[i],
       });
-      init++
+      init++;
     }
   }
-  return { generatedArray, num }
-}
+  return { generatedArray, num };
+};
 
-const PickerComp = ({ items, value, onChangeValue, onLoadMore }: PickerCompProps) => (
+const PickerComp = ({
+  items,
+  value,
+  onChangeValue,
+}: // onLoadMore,
+PickerCompProps) => (
   <Picker
     selectedValue={value}
     onValueChange={onChangeValue}
-    onScrollChange={
-      throttle(
-        (scrollHeight) => {
-          console.log('%c scrollHeight: ', 'background-color:purple; color:#FFF;', scrollHeight)
-          if (scrollHeight <= 20) {
-            console.log('%c items[0].label: ', 'background-color:purple; color:#FFF;', items[0].label)
-            onLoadMore('prev')
-          } else if (scrollHeight >= items.length - 20) {
-            console.log('%c items.length - 200: ', 'background-color:purple; color:#FFF;', items.length - 200)
-            onLoadMore('next')
-          }
-        },
-        2000,
-        { trailing: false }
-      )        
-    }
+    // onScrollChange={
+    //   throttle(
+    //     (scrollHeight) => {
+    //       console.log('%c scrollHeight: ', 'background-color:purple; color:#FFF;', scrollHeight)
+    //       if (scrollHeight <= 20) {
+    //         console.log('%c items[0].label: ', 'background-color:purple; color:#FFF;', items[0].label)
+    //         onLoadMore('prev')
+    //       } else if (scrollHeight >= items.length - 20) {
+    //         console.log('%c items.length - 200: ', 'background-color:purple; color:#FFF;', items.length - 200)
+    //         onLoadMore('next')
+    //       }
+    //     },
+    //     2000,
+    //     { trailing: false }
+    //   )
+    // }
   >
     {items.map(({ label, datum }, index) => (
       <Picker.Item key={index} value={label}>
@@ -71,127 +79,142 @@ const PickerComp = ({ items, value, onChangeValue, onLoadMore }: PickerCompProps
       </Picker.Item>
     ))}
   </Picker>
-)
+);
 
 export default class SlotMachine extends PureComponent<slotMachineProps> {
   state = {
     items: [],
-    value: 0
-  }    
+    value: 0,
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    const { items: { length: itemsLength }} = this.state
-    const { items: { length: prevItemsLength }} = prevState
+    const {
+      items: { length: itemsLength },
+    } = this.state;
+    const {
+      items: { length: prevItemsLength },
+    } = prevState;
     if (itemsLength !== prevItemsLength) {
-      console.log('%c prevItemsLength: ', 'background-color:purple; color:#FFF;', prevItemsLength)
-      console.log('%c itemsLength: ', 'background-color:magenta; color:#FFF;', itemsLength)
+      console.log(
+        "%c prevItemsLength: ",
+        "background-color:purple; color:#FFF;",
+        prevItemsLength
+      );
+      console.log(
+        "%c itemsLength: ",
+        "background-color:magenta; color:#FFF;",
+        itemsLength
+      );
     }
   }
 
-  onReset = cbFunc => {
-    this.setState({ items: [] }, cbFunc)
-  }
+  onReset = (cbFunc) => {
+    this.setState({ items: [] }, cbFunc);
+  };
 
   onItemsCalculate = async (order, more = false) => {
-    const { num, ...processedData } = await this.dataPrepare()
-    const { length: itemsLen } = this.state.items
-    let newNum = itemsLen + num
-    if (order === 'prev') {
-      newNum = -(Math.abs(itemsLen) + num)
+    const { num, ...processedData } = await this.dataPrepare();
+    const { length: itemsLen } = this.state.items;
+    let newNum = itemsLen + num;
+    if (order === "prev") {
+      newNum = -(Math.abs(itemsLen) + num);
     }
-    return await generateArray({ num: newNum, ...processedData, more })
-  }
+    return await generateArray({ num: newNum, ...processedData, more });
+  };
 
   onReady = () => {
-    Keyboard.dismiss()
+    Keyboard.dismiss();
     this.onReset(() => {
       this.onItemsCalculate().then(({ generatedArray, num }) => {
-        const itemsLength = generatedArray.length
-        const value = this.calibrateValue(itemsLength, num)
-        this.setState({ value, items: generatedArray })
-      })      
-    })
-  }
+        const itemsLength = generatedArray.length;
+        const value = this.calibrateValue(itemsLength, num);
+        this.setState({ value, items: generatedArray });
+      });
+    });
+  };
 
   onOk = () => {
-    const { items, value } = this.state
-    const { [value]: { datum } = {}} = items
-    this.props.answer(datum)  
-  }
+    const { items, value } = this.state;
+    const { [value]: { datum } = {} } = items;
+    this.props.answer(datum);
+  };
 
-  onChangeValue = value => {
-    this.setState({ value })
-  }
+  onChangeValue = (value) => {
+    this.setState({ value });
+  };
 
   onLoadMore = async (order) => {
-    const { generatedArray } = await this.onItemsCalculate(order, true)
-    console.log('generatedArray: ', generatedArray)
-    this.setState(prevState => {
-      if (order === 'prev') {
-        return { 
-          items: [
-            ...generatedArray,
-            ...prevState.items
-          ]   
-        }     
-      } else {
-        return {
-          items: [
-            ...prevState.items,
-            ...generatedArray
-          ]
+    const { generatedArray } = await this.onItemsCalculate(order, true);
+    console.log("generatedArray: ", generatedArray);
+    this.setState(
+      (prevState) => {
+        if (order === "prev") {
+          return {
+            items: [...generatedArray, ...prevState.items],
+          };
+        } else {
+          return {
+            items: [...prevState.items, ...generatedArray],
+          };
         }
-      }  
-    }, () => {
-      console.log('after setState of items on onLoadMore: ', this.state.items)
-    })
-  }
+      },
+      () => {
+        console.log(
+          "after setState of items on onLoadMore: ",
+          this.state.items
+        );
+      }
+    );
+  };
 
   calibrateValue = (dataLength, num) => {
     let calcNum;
     if (num === 0) {
-      calcNum = 1
+      calcNum = 1;
     } else {
-      calcNum = num
+      calcNum = num;
     }
-    const comp = Math.floor(Math.floor(dataLength / calcNum) * calcNum / 2)
-    return comp - comp % calcNum
-  }
+    const comp = Math.floor((Math.floor(dataLength / calcNum) * calcNum) / 2);
+    return comp - (comp % calcNum);
+  };
 
   dataPrepare = async () => {
-    const { option, items, numRange: [from, to] } = this.props
-    let data, num, startIdx, endIdx, limit
-    if (option !== 'numbers') {
-      const refinedItems = option ==='custom' 
-        ? items.slice(0, -1)
-        : items
-      data = await refinedItems.map(item => item.value)
-      num = data.length
-      startIdx = 0
-      endIdx = num
-      limit = num === 0 ? MAX_ITEM_LIST_NUM : Math.floor(MAX_ITEM_LIST_NUM / num)
+    const {
+      option,
+      items,
+      numRange: [from, to],
+    } = this.props;
+    let data, num, startIdx, endIdx, limit;
+    if (option !== "numbers") {
+      const refinedItems = option === "custom" ? items.slice(0, -1) : items;
+      data = await refinedItems.map((item) => item.value);
+      num = data.length;
+      startIdx = 0;
+      endIdx = num;
+      limit =
+        num === 0 ? MAX_ITEM_LIST_NUM : Math.floor(MAX_ITEM_LIST_NUM / num);
     } else {
-      num = Math.abs(to - from) + 1
-      startIdx = Number(from)
-      endIdx = Number(to)
+      num = Math.abs(to - from) + 1;
+      startIdx = Number(from);
+      endIdx = Number(to);
       if (endIdx === startIdx) {
-        limit = MAX_ITEM_LIST_NUM
+        limit = MAX_ITEM_LIST_NUM;
       } else {
-        limit = Math.floor(MAX_ITEM_LIST_NUM / Math.abs(endIdx - startIdx))
-      }    
-      data = await [...Array(endIdx + 1).keys()].filter(key => (
-        ![...Array(startIdx).keys()].includes(key)
-      ))
+        limit = Math.floor(MAX_ITEM_LIST_NUM / Math.abs(endIdx - startIdx));
+      }
+      data = await [...Array(endIdx + 1).keys()].filter(
+        (key) => ![...Array(startIdx).keys()].includes(key)
+      );
     }
-    return { data, startIdx, endIdx, limit, num }
-  }
+    return { data, startIdx, endIdx, limit, num };
+  };
 
   render() {
-    const { disabled } = this.props
-    const { items, value } = this.state    
-    return (      
+    const { disabled } = this.props;
+    const { items, value } = this.state;
+    return (
       <ScrollView>
-        <View style={styles.container}>         
+        <View style={styles.container}>
           <Popup
             transitionName="rmc-picker-popup-slide-fade"
             maskTransitionName="rmc-picker-popup-fade"
@@ -208,24 +231,24 @@ export default class SlotMachine extends PureComponent<slotMachineProps> {
             onDismiss={this.onDismiss}
             disabled={disabled}
             triggerType="onPressOut"
-          >       
+          >
             <BottomButton
               title="Ready"
               onPress={this.onReady}
               confirmColor="#444"
               disabled={disabled}
             />
-          </Popup>            
-        </View>          
-      </ScrollView>        
-    )
+          </Popup>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    overflow: 'hidden'
-  }
-})
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+});
